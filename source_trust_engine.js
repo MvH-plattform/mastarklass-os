40389a703600c6c890c14ad6772ed66f1a642835
MvH-plattform <scherling87@hotmail.com>
2026-07-14 00:20:36 +0200

    Delete source_trust_engine.js

--- source_trust_engine.js
+++
@@ -1,8 +0,0 @@
-
-window.MKSourceTrust85={
- list(){return window.DATA?.verifiedData85?.sourceRatings||[];},
- approved(){return this.list().filter(x=>x.status==="GODKÄND FÖR TEST");},
- best(category){
-   return this.list().filter(x=>!category||x.category===category).sort((a,b)=>b.totalScore-a.totalScore)[0]||null;
- }
-};
