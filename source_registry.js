2025700893d0f895b6f041d342c69f6fa5a35c7f
MvH-plattform <scherling87@hotmail.com>
2026-07-14 00:20:02 +0200

    Delete source_registry.js

--- source_registry.js
+++
@@ -1,9 +0,0 @@
-
-window.MKSourceRegistry={
- list(){return (window.DATA?.liveFoundation81?.sources||[]).map(x=>({...x}));},
- enabled(){return this.list().filter(x=>x.enabled);},
- get(id){return this.list().find(x=>x.id===id)||null;},
- canActivate(source){
-   return !!source && source.licenseStatus==="GODKÄND" && source.corsStatus==="GODKÄND" && source.adapterStatus==="TESTAD";
- }
-};
