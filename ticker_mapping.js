2de2c6976628c97c71b10012675c9d5c5f260947
MvH-plattform <scherling87@hotmail.com>
2026-07-13 04:56:55 +0200

    Delete ticker_mapping.js

--- ticker_mapping.js
+++
@@ -1,8 +0,0 @@
-
-window.MKTickerMapping82={
- records(){return window.DATA?.livePortfolio82?.records||[];},
- summary(){
-   const r=this.records();
-   return {total:r.length,mapped:r.filter(x=>x.ticker).length,ready:r.filter(x=>x.mappingStatus==="READY").length};
- }
-};
