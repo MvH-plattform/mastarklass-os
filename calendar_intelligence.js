df7506d49b365aad23dac8f856a529d044d55abb
MvH-plattform <scherling87@hotmail.com>
2026-07-13 23:51:38 +0200

    Delete calendar_intelligence.js

--- calendar_intelligence.js
+++
@@ -1,6 +0,0 @@
-
-window.MKCalendarIntelligence84={
- items(){return window.DATA?.marketConnect83?.calendarItems||[];},
- dated(){return this.items().filter(x=>x.date).slice().sort((a,b)=>String(a.date).localeCompare(String(b.date)));},
- undated(){return this.items().filter(x=>!x.date);}
-};
