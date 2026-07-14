ee591d58fcb046ae0b705f5d42a783df18d3a424
MvH-plattform <scherling87@hotmail.com>
2026-07-14 10:27:08 +0200

    Delete market_reasoning_engine.js

--- market_reasoning_engine.js
+++
@@ -1,6 +0,0 @@
-
-window.MKMarketReasoning84={
- signals(){return window.DATA?.marketIntelligence84?.signals||[];},
- byImpact(impact){return this.signals().filter(x=>x.impact===impact);},
- facts(){return this.signals().filter(x=>x.classification==="FAKTA"||x.classification==="LOKAL DATA");}
-};
