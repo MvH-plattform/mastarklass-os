74f35afd3f84dd5095495ed15a1c19a8a2c35d34
MvH-plattform <scherling87@hotmail.com>
2026-07-18 21:09:12 +0200

    Delete legal_compliance.js

--- legal_compliance.js
+++
@@ -1,6 +0,0 @@
-
-window.MKLegalCompliance85={
- checks(){return window.DATA?.verifiedData85?.complianceChecks||[];},
- allPassed(){return this.checks().every(x=>x.status==="PASS");},
- blockers(){return this.checks().filter(x=>x.status!=="PASS");}
-};
